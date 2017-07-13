c6e7e0213c626ce092be417ab7bcee1cc140e44d
Beth Hansen and Kate Byars <kate@>
2017-07-13 10:52:38 -0700

    add index 0 has 3 working conditions

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 $(document).ready(function() {
+var romanNumerals = [];
 
   $("form.romanNumeral").submit(function(event) {
     event.preventDefault();
     var input = $("input:text").val();
-    alert(input);
-    var splitInput = input.split("");
-    alert(splitInput);
+    // alert(input);
+    var splitInput = input.split("");//this will take user input and return it to a split array
+    var zero = splitInput.slice(0,1);
+    alert(zero);
+    var one = splitInput.slice(0,1)
+    var two = splitInput.slice(0,1)
+    var three = splitInput.slice(0,1)
 
-  //   function makeArray(number) {
-  //     alert("hi");
-  //   var splitInput = number.split("");
-  // };
-
-    // alert(makeArray(input));
+    // alert(splitInput);
+    if (zero == "1") {
+      romanNumerals.push("M");
+      // alert(romanNumerals);
+    } else if (zero == "2") {
+      romanNumerals.push("MM");
+    } else if (zero == "3") {
+      romanNumerals.push("MMM");
+      alert(romanNumerals);
+    }
   });
 });
